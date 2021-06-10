53001ccafa320d9f74bcaa6ef5fce4724a6a11d4
Vebjørn <70209955+VebOpem@users.noreply.github.com>
2021-06-10 17:54:30 +0200

    oblig 3

---
+++ oblig-3 – Kopi/js-files/storage.js
@@ -0,0 +1,14 @@
+console.log("linked storage");
+/*
+function store() {
+    let storeName = ;
+    let storeAge = ;
+    let storeTlf = ;
+    let storeEmail = ;
+};
+function get() {
+
+};
+function remove() {
+
+};*/
